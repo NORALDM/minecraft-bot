51f56b43bf257ee7d02f6dc2731317076fc9f8e4
Your Name <you@example.com>
2025-02-07 00:59:46 +0530

    not the last changes :(

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@ echo 1 >> track
 git add track
 git commit -m "$(wc -l track|awk '{print $1}')"
 git push
-while [ $(curl https://gist.githubusercontent.com/NORALDM/f0fd68f8d3f658a3911eabf47b23a9d0/raw/log.txt | awk '{print $NF}') = $(cat last-auth | tee last-auth.1) ]; do
+url=$({curl -sf https://gist.githubusercontent.com/NORALDM/f0fd68f8d3f658a3911eabf47b23a9d0/raw/log.txt > tmp.log && awk '{print $NF}' tmp.log || cat /dev/null }; rm tmp.log )
+if [ -z $url ]; then return 1; fi
+while [ $url  = $(cat last-auth) ]; do
 	sleep 1;
 done
 mv last-auth.1 last-auth
